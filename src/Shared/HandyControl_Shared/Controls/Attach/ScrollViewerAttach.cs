e3456448ee86c749c4640a019d310dd7e75d05bf
DESKTOP-LFC\lfc <lfc_lj@foxmail.com>
2022-05-22 01:02:09 +0800

    保留兼容

--- src/Shared/HandyControl_Shared/Controls/Attach/ScrollViewerAttach.cs
+++ src/Shared/HandyControl_Shared/Controls/Attach/ScrollViewerAttach.cs
@@ -10,46 +10,47 @@ namespace HandyControl.Controls
         public static readonly DependencyProperty AutoHideProperty = DependencyProperty.RegisterAttached(
             "AutoHide", typeof(bool), typeof(ScrollViewerAttach), new FrameworkPropertyMetadata(ValueBoxes.TrueBox, FrameworkPropertyMetadataOptions.None));
 
-    public static void SetAutoHide(DependencyObject element, bool value)
-        => element.SetValue(AutoHideProperty, ValueBoxes.BooleanBox(value));
+        public static void SetAutoHide(DependencyObject element, bool value)
+            => element.SetValue(AutoHideProperty, ValueBoxes.BooleanBox(value));
 
-    public static bool GetAutoHide(DependencyObject element)
-        => (bool) element.GetValue(AutoHideProperty);
+        public static bool GetAutoHide(DependencyObject element)
+            => (bool) element.GetValue(AutoHideProperty);
 
         public static readonly DependencyProperty OrientationProperty = DependencyProperty.RegisterAttached(
             "Orientation", typeof(Orientation), typeof(ScrollViewerAttach), new FrameworkPropertyMetadata(ValueBoxes.VerticalBox, FrameworkPropertyMetadataOptions.None, OnOrientationChanged));
 
-    private static void OnOrientationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        if (d is ScrollViewer)
+        private static void OnOrientationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            return;
+            if (d is ScrollViewer)
+            {
+                return;
+            }
+
+            if (d is System.Windows.Controls.ScrollViewer scrollViewer)
+            {
+                if ((Orientation) e.NewValue == Orientation.Horizontal)
+                {
+                    scrollViewer.PreviewMouseWheel += ScrollViewer_PreviewMouseWheel;
+                }
+                else
+                {
+                    scrollViewer.PreviewMouseWheel -= ScrollViewer_PreviewMouseWheel;
+                }
+            }
         }
 
-        if (d is System.Windows.Controls.ScrollViewer scrollViewer)
+        private static void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
         {
-            if ((Orientation) e.NewValue == Orientation.Horizontal)
-            {
-                scrollViewer.PreviewMouseWheel += ScrollViewer_PreviewMouseWheel;
-            }
-            else
-            {
-                scrollViewer.PreviewMouseWheel -= ScrollViewer_PreviewMouseWheel;
-            }
+            var scrollViewer = (System.Windows.Controls.ScrollViewer) sender;
+            scrollViewer.ScrollToHorizontalOffset(Math.Min(Math.Max(0, scrollViewer.HorizontalOffset - e.Delta), scrollViewer.ScrollableWidth));
+
+            e.Handled = true;
         }
+
+        public static void SetOrientation(DependencyObject element, Orientation value)
+            => element.SetValue(OrientationProperty, ValueBoxes.OrientationBox(value));
+
+        public static Orientation GetOrientation(DependencyObject element)
+            => (Orientation) element.GetValue(OrientationProperty);
     }
-
-    private static void ScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
-    {
-        var scrollViewer = (System.Windows.Controls.ScrollViewer) sender;
-        scrollViewer.ScrollToHorizontalOffset(Math.Min(Math.Max(0, scrollViewer.HorizontalOffset - e.Delta), scrollViewer.ScrollableWidth));
-
-        e.Handled = true;
-    }
-
-    public static void SetOrientation(DependencyObject element, Orientation value)
-        => element.SetValue(OrientationProperty, ValueBoxes.OrientationBox(value));
-
-    public static Orientation GetOrientation(DependencyObject element)
-        => (Orientation) element.GetValue(OrientationProperty);
 }
